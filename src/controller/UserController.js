d5c260ce33e91311b8d6d13329ed7bde5b93a50d
dungggg1203 <dungcongnghiep4@gmail.com>
2023-09-30 00:13:41 +0700

    api_user

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,9 +1,28 @@
 const UserService = require('../services/UserService')
 const createUser = async (req,res) => {
     try{
-        console.log(req.body);
-         const res = await UserService.createUser
-         return res.status(200).json(res)
+        const {name ,email,password,confirmPassword,phone} = req.body
+         const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+         const isCheckEmail = reg.test(email)
+        if(!name || !email || !password || !confirmPassword ||!phone){
+            return res.status(200).json({
+                status:'ERR',
+                message:'Insufficient value entered'
+            })
+        }else if(!isCheckEmail){
+            return res.status(200).json({
+                status:'ERR',
+                message:'Data is not email'
+            })
+        }else if(password !== confirmPassword){
+            return res.status(200).json({
+                status:'ERR',
+                message:'Password unlike confirmPassword'
+            })
+        }
+         const response = await UserService.createUser(req.body) // nếu k rơi vào trường hợp nào thì cho 
+         // req.body qua thằng UserService
+         return res.status(200).json(response)
     }catch(e){
         return res.status(404).json({
             message:e
